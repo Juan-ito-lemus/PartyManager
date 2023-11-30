d6b03e1c67fc5880675b3b8016e23d126e73e291
Juan Lemus Garcia <lemus9600@gmail.com>
2023-11-30 13:52:45 -0600

    Actualice la base de datos y eliminamos vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- sss -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 <head>
     <!-- ... (omitiendo el contenido de la etiqueta <head> para simplificar) ... -->
 </head>
