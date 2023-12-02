ad902446c5505f224bbdaf735e7281cacce2e5f9
Juan Lemus Garcia <lemus9600@gmail.com>
2023-12-01 21:53:19 -0600

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      refresh: true,
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: ['algoliasearch-helper'],
-      // Añade aquí cualquier otro módulo externo si es necesario
-    },
-  },
-  server: {
-    // Agrega la configuración del servidor para Vite
-    proxy: {
-      '/': {
-        target: 'http://localhost', // Cambia esto según la configuración de tu aplicación Laravel
-        ws: true,
-      },
-    },
-  },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
