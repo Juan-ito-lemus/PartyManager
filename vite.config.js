c0b4bdea825ca6ad6948ec119c52f905c5cf8175
Juan Lemus Garcia <lemus9600@gmail.com>
2023-12-01 21:39:03 -0600

    Config. Nixpacks

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,28 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            external: ['algoliasearch-helper']
-            // Añade aquí cualquier otro módulo externo si es necesario
-        },
-    },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['algoliasearch-helper'],
+      // Añade aquí cualquier otro módulo externo si es necesario
+    },
+  },
+  server: {
+    // Agrega la configuración del servidor para Vite
+    proxy: {
+      '/': {
+        target: 'http://localhost', // Cambia esto según la configuración de tu aplicación Laravel
+        ws: true,
+      },
+    },
+  },
 });
